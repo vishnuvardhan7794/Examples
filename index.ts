2218ae5b62601376a0bc6f8642fed83f8ccd4434
vishnuvardhan7794 <59390180+vishnuvardhan7794@users.noreply.github.com>
2023-01-11 18:07:53 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
 import './style.css';
 
-import { of, map, Observable } from 'rxjs';
+import { of, Observable, interval, MonoTypeOperatorFunction, pipe } from 'rxjs';
+import { tap, take, map } from 'rxjs/operators';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
+// export function log<T>(): (source$: Observable<T>) => Observable<T> {
+//   return (source$) => source$.pipe(tap(console.log));
+// }
+const source$: Observable<string> = of('asd', 'asd', 'asf').pipe(take(3));
+
+// export function identity<T>(source$:Observable<T>):Observable<T>{
+//   return source$.pipe(tap(console.log));
+// }
+export function logWithTag<T>(tag: string): MonoTypeOperatorFunction<T> {
+  return pipe(tap((value) => console.log(`${tag}:${value}`)));
+}
+source$.pipe(logWithTag('vishnu')).subscribe((value) => {
+  console.log(`Testing :${value}`);
+});
 
 // Open the console in the bottom right to see results.
